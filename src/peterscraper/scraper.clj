9dd22970ddacb8b94c9d15ff7a5addab7bbe347d
Oliver Mooney <oliver.mooney@gmail.com>
2014-12-16 12:02:17 +0000

    Quick slurp-url defn to check everything's present & correct

--- src/peterscraper/scraper.clj
+++ src/peterscraper/scraper.clj
@@ -1,9 +1,8 @@
 (ns peterscraper.scraper
-  (:import (java.net URL))
   (:require [net.cgrand.enlive-html :as html]
             [clj-http.client :as client]
             [aprint.core :refer :all]))
 
-(defn fetch-url [url]
-  (html/html-resource (URL. url)))
+(defn slurp-url [url]
+  (html/html-snippet (:body (client/get url))))
 
