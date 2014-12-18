637b5c73244bae0d1ed84f9d18bdf581cd61c7bb
Oliver Mooney <Oliver.Mooney@gmail.com>
2014-12-18 13:55:41 +0000

    REPL testing artifacts

--- src/peterscraper/scraper.clj
+++ src/peterscraper/scraper.clj
@@ -6,3 +6,48 @@
 (defn slurp-url [url]
   (html/html-snippet (:body (client/get url))))
 
+(def post-params {:simostrar       "no"
+                  :codaut          "00"
+                  :tipoOperacion   nil
+                  :codprov         "00"
+                  :codcen          "04000018"
+                  :codcen2         "04000018"
+                  :denomiespe      nil
+                  :ssel_natur      "0"
+                  :sconcerta       "0"
+                  :tipocentro      "0"
+                  :combosub        nil
+                  :combomuni       nil
+                  :comboloc        nil
+                  :comboniv        "131"
+                  :combogra        nil
+                  :comboens        "-1"
+                  :combofam        nil
+                  :combopais       nil
+                  :comboislas      nil
+                  :comboprovin     nil
+                  :nombreaut       nil
+                  :nombrepro       nil
+                  :modalidad       "0"
+                  :textotipocentro "Todos"
+                  :textosub        nil
+                  :textomunicipio  nil
+                  :textolocalidad  nil
+                  :textonivel      "Educaci%F3n+Secundaria+Obligatoria"
+                  :textogrado      nil
+                  :textoensenanza  "Todos"
+                  :textofamilia    "Todos"
+                  :textopais       nil
+                  :textonaturaleza "Todos"
+                  :textoconcertado "Todos"
+                  :textomodalidad  "Todas"
+                  })
+
+(defn post-params-test
+  (client/post "https://www.educacion.gob.es/centros/saccen.do"
+                                    {:insecure?   true
+                                     :form-params post-params}))
+
+(def test-snippet (post-params-test))
+
+(html/select testsnippet [:li])
